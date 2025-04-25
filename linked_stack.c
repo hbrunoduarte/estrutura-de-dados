0a43cd5671cddfe348d3bee67aa2ccf65cc4bf59
Bruno Duarte <hbrunoduarte@gmail.com>
2025-04-25 13:48:46 -0300

    adicionadas pilha estatica e pilha encadeada

---
+++ linked_stack.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+Stack* createStack();
+void push(Stack* stack, int value);
+int pop(Stack* stack);
+int peek(Stack* stack);
+void resetStack(Stack* stack);
+int isEmpty(Stack* stack);
+
+typedef struct node {
+    int value;
+    Node* link;
+} Node;
+
+typedef struct stack {
+    int size;
+    Node* top;
+} Stack;
+
+Stack* createStack() {
+    Stack* newStack = malloc(sizeof(Stack));
+    newStack->top = NULL;
+    newStack->size = 0;
+
+    return newStack;
+}
+
+void push(Stack* stack, int value) {
+    Node* node = malloc(sizeof(Node));
+    node->value = value;
+    
+    node->link = stack->top;
+    stack->top = node;
+    stack->size++;
+}
+
+int pop(Stack* stack) {
+    if(isEmpty(stack)) {
+            printf("Stack Underflow\n");
+            return 0;
+    } else {
+            Node* aux = stack->top;
+            int value = aux->value;
+            
+            stack->top = aux->link;;
+            free(aux);
+            stack->size--;
+            
+            return value;
+    }
+}
+
+int peek(Stack* stack) {
+    if(isEmpty(stack)) {
+            printf("Stack is empty\n");
+            return 0;
+    } else {
+            return stack->top->value;
+    }
+}
+
+void resetStack(Stack* stack) {
+    while(!isEmpty(stack)) {
+            pop(stack);
+    }
+}
+
+int isEmpty(Stack* stack) {
+    if(stack->top == NULL) {
+            return 1;
+    } else {
+            return 0;
+    }
+}
---
+++ static_stack.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct stack {
+    int top; //index
+    int size;    
+    int* array;
+} Stack;
+
+Stack* createStack(int size);
+void push(Stack* stack, int value);
+int pop(Stack* stack);
+int peek(Stack* stack);
+void resetStack(Stack* stack);
+int isFull(Stack* stack);
+int isEmpty(Stack* stack);
+
+Stack* createStack(int size) {
+    Stack* newStack = malloc(sizeof(Stack));
+    newStack->array = malloc(sizeof(int)*size);
+    newStack->top = -1;
+    newStack->size = size;
+
+    return newStack;
+}
+
+void push(Stack* stack, int value) {
+    if(isFull(stack)) {
+            printf("Stack Overflow\n");
+            return;
+    } else {
+            stack->top++;
+            stack->array[stack->top] = value;
+    }
+}
+
+int pop(Stack* stack) {
+    if(isEmpty(stack)) {
+            printf("Stack Underflow\n");
+            return 0;
+    } else {
+            int value = stack->array[stack->top];
+            stack->top--;
+            return value;
+    }
+}
+
+int peek(Stack* stack) {
+    if(isEmpty(stack)) {
+            printf("Stack is empty\n");
+            return 0;
+    } else {
+            return stack->array[stack->top];
+    }
+}
+
+void resetStack(Stack* stack) {
+    stack->top = -1;
+}
+
+int isFull(Stack* stack) {
+    if(stack->top == stack->size) {
+            return 1;
+    } else {
+            return 0;
+    }
+}
+
+int isEmpty(Stack* stack) {
+    if(stack->top == -1) {
+            return 1;
+    } else {
+            return 0;
+    }
+}
